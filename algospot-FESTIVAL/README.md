c0f3b3add41de5c926dc3d120b684372d2bbb6c1
audgur <123audgur123@naver.com>
2023-06-15 23:20:49 +0900

    solved FESTIVAL

--- algospot-FESTIVAL/README.md
+++ algospot-FESTIVAL/README.md
@@ -1,6 +1,6 @@
 https://algospot.com/judge/problem/read/FESTIVAL
 
-# 풀이
+# Solving
 
 * 방법 1 - 무식하게 계산하는 방법
 1. 먼저 기준이 되는날 P를 잡는다.
@@ -8,3 +8,12 @@ https://algospot.com/judge/problem/read/FESTIVAL
 3. P의 값이 1 부터 시작해서 N-L+1까지 과정 1,2를 반복해서 평균이 가장 낮은 값을 찾는다.
 
 O(n) = n(n+1) = n<sup>2</sup>
+
+
+# Learning
+
+* cout의 소수 표현법에 정확도가 달라진다.
+* cout 사용시 소수점 아래로 8자리까지 표시하려면 :
+` cout << fixed `
+` cout.precision(8) `
+단 9자리에서 반올림 됨
--- algospot-FESTIVAL/solution.cpp
+++ algospot-FESTIVAL/solution.cpp
@@ -4,5 +4,42 @@ using namespace std;
 
 int main()
 {
-	cout << "im solution\n";
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	cout << fixed;
+	cout.precision(8);
+
+	int cost[1001];
+	int T;
+	cin >> T;
+	for (int test_case = 0; test_case < T; test_case++)
+	{
+		int N, L;
+		cin >> N >> L;
+		for (int i = 0; i < N; i++)
+			cin >> cost[i];
+
+		double min = 100000., dL = (double)L;
+		for (int i = 0; i < N - L + 1; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < L; j++)
+				sum += cost[i + j];
+			
+			double tp = (double)sum / dL;
+			if (tp < min)
+				min = tp;
+			for (int j = 1; j < N - i - L + 1; j++)
+			{
+				sum += cost[i + L + j - 1];
+				tp = (double)sum / (dL + (double)j);
+				if (tp < min)
+					min = tp;
+			}
+		}
+
+		cout << min << '\n';
+	}
 }
