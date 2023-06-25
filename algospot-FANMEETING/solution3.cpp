39acf836d6769d7554c12ba9c397c2c2587ef608
audgur <123audgur123@naver.com>
2023-06-26 03:06:45 +0900

    FANMEETING solution3

--- algospot-FANMEETING/solution3.cpp
+++ algospot-FANMEETING/solution3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef unsigned long long ULL;
 
 void DataToBit(vector<ULL>&, string&);
-bool BigAndOper(vector<ULL>&, vector<ULL>&,int,int);
+bool BigAndOper(vector<ULL>&, vector<ULL>&, int, int);
 void BigBitShift(vector<ULL>&);
 
 int main()
@@ -28,17 +28,17 @@ int main()
 			if (str[i] == 'M')
 				for (int shift = 0; shift < 64; ++shift)
 					member_bit[shift][(i + shift) / 64] |= 1ULL << ((i + shift) & 63);
-		int member_bit_sz = (member_sz + 62)/64 + 1;
+		int member_bit_sz = (member_sz + 62) / 64 + 1;
 
 		cin >> str;
 		fan_sz = str.size();
 		for (int i = 0; i < fan_sz; ++i)
 			if (str[i] == 'M')
-				fan_bit[i / 64] |= 1ULL << (i&63);
+				fan_bit[i / 64] |= 1ULL << (i & 63);
 
 		int hugs(0);
 		int loop = fan_sz - member_sz + 1;
-		for (int i = 0; i < loop; ++i) 
+		for (int i = 0; i < loop; ++i)
 			hugs += BigAndOper(member_bit[i & 63], fan_bit, member_bit_sz, i / 64);
 
 		cout << hugs << '\n';
@@ -49,7 +49,7 @@ int main()
 bool BigAndOper(vector<ULL>& a, vector<ULL>& b, int a_sz, int b_off)
 {
 	for (int i = 0; i < a_sz; ++i) {
-		if (a[i] & b[i+b_off]) return false;
+		if (a[i] & b[i + b_off]) return false;
 	}
 	return true;
 }
