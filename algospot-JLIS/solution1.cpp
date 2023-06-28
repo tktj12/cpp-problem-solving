81b342230b81ca45b315b7a074b93071dbf41f95
audgur <123audgur123@naver.com>
2023-06-29 06:04:27 +0900

    JLIS solution1

---
+++ algospot-JLIS/solution1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+int LenOfJLIS(int, int);
+
+int cache[101][101];
+vector<long long> A, B;
+int main()
+{
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		memset(cache, 0, sizeof(cache));
+		int n, m;
+		cin >> n >> m;
+		A = vector<long long>(n+1);
+		B = vector<long long>(m+1);
+		A[0] = B[0] = -0xFFFFFFFFLL;
+		for (int i = 1; i <= n; ++i) cin >> A[i];
+		for (int i = 1; i <= m; ++i) cin >> B[i];
+		cout << LenOfJLIS(0, 0)-1 << '\n';
+	}
+	return 0;
+}
+
+int LenOfJLIS(int a, int b)
+{
+	int& ret = cache[a][b];
+	if (ret) return ret;
+
+	ret = 1;
+	long long tail = max(A[a], B[b]);
+	for (int i = a + 1; i < A.size(); ++i)
+		if (A[i] > tail)
+			ret = max(ret, 1+LenOfJLIS(i, b));
+
+	for (int i = b + 1; i < B.size(); ++i)
+		if (B[i] > tail)
+			ret = max(ret, 1+LenOfJLIS(a, i));
+	
+	return ret;
+}
