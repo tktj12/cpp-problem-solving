c5ed36249d9b5dffecbab5f1a3ef252465f46607
audgur <123audgur123@naver.com>
2023-08-16 07:18:25 +0900

    BOJ-13264 solution1,2

--- BOJ-13264/solution2.cpp
+++ BOJ-13264/solution2.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class SuffixComparator {
 public:
 	int t;
-	vector<int> group;
+	vector<int>& group;
 	SuffixComparator(int t, vector<int>& group) : t(t), group(group) {}
 
 	bool operator()(int a, int b) {
@@ -18,27 +18,58 @@ public:
 	}
 };
 
+void CountingSort(vector<int>& perm, vector<int>& group, int t)
+{
+	int n = perm.size(), max_num = 100010;
+	// i's first = perm[i], i's second = perm[i]+t 
+	vector<int> result(n), it_has_sorted_idx_by_second(n), cnt(max_num, 0);
+	for (int i = 0; i < n; i++) {
+		int index_in_original_str = perm[i] + t;
+		int group_num = group[index_in_original_str];
+		cnt[group_num]++;
+	}
+	for (int i = 1; i < max_num; i++) cnt[i] += cnt[i - 1];
+	for (int i = n - 1; i >= 0; i--) {
+		int group_num = group[perm[i] + t]; // skip : index_in_original_str
+		int order_of_second = --cnt[group_num];
+		it_has_sorted_idx_by_second[order_of_second] = i;
+	}
+
+	cnt.assign(max_num, 0);
+	for (int i = 0; i < n; i++) cnt[group[perm[i]]]++; // skip : group_num
+	for (int i = 1; i < max_num; i++) cnt[i] += cnt[i - 1];
+	for (int i = n - 1; i >= 0; i--) {
+		int index_in_perm = it_has_sorted_idx_by_second[i];
+		int index_in_original_str = perm[index_in_perm];
+		int group_num = group[index_in_original_str];
+		int order_of_first = --cnt[group_num];
+		result[order_of_first] = perm[index_in_perm];
+	}
+
+	perm = result;
+}
+
 vector<int> perm;
 void ManberMyers(string& str)
 {
 	int n = str.size();
-	vector<int> group(n+1);
+	vector<int> group(2*n, 0);
 	for (int i = 0; i < n; i++) group[i] = str[i];
-	group[n] = -1;
 
 	perm = vector<int>(n);
 	for (int i = 0; i < n; i++) perm[i] = i;
 	int t = 1;
 	while (true) {
 		SuffixComparator compare_using_2t(t, group);
-		sort(perm.begin(), perm.end(), compare_using_2t);
+		//sort(perm.begin(), perm.end(), compare_using_2t);
+		CountingSort(perm, group, t);
+
 		t *= 2;
 		if (t >= n) break;
 
-		vector<int> new_group(n + 1);
-		new_group[n] = -1;
+		vector<int> new_group(2*n, 0);
 
-		new_group[perm[0]] = 0;
+		new_group[perm[0]] = 1;
 		for (int i = 1; i < n; i++) {
 			if (compare_using_2t(perm[i - 1], perm[i])) {
 				new_group[perm[i]] = new_group[perm[i - 1]] + 1;
@@ -77,11 +108,6 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 	ios::sync_with_stdio(false);
-
-	/*if (freopen("C:\\Users\\123au\\cpp-problem-solving\\input.txt", "r", stdin) == nullptr)
-		return 1;
-	if (freopen("C:\\Users\\123au\\cpp-problem-solving\\output.txt", "w", stdout) == nullptr)
-		return 1;*/
 
 	string str;
 	cin >> str;
