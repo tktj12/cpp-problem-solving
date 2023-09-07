2f0c35a4052cf6e4a82c864cadb1bbd5b367d51d
audgur <123audgur123@naver.com>
2023-09-07 18:03:52 +0900

    BOJ-17436 solution 1

--- BOJ-17436/solution1.cpp
+++ BOJ-17436/solution1.cpp
@@ -2,7 +2,25 @@
 #include <vector>
 #include <cstring>
 using namespace std;
+using ull = unsigned long long;
+using ll = long long;
 
+int N, prime[10];
+ll M, ans;
+void Solve(int depth, int choose, ull num)
+{
+	if (depth == N) {
+		if (choose == 0) return;
+
+		ll tp = M/num;
+		if (choose % 2 == 0) tp = -tp;
+		ans += tp;
+		return;
+	}
+
+	Solve(depth + 1, choose + 1, num * prime[depth]);
+	Solve(depth + 1, choose, num);
+}
 
 int main()
 {
@@ -10,10 +28,11 @@ int main()
 	cout.tie(nullptr);
 	ios::sync_with_stdio(false);
 
-	int tc;
-	cin >> tc;
-	while (tc--) {
-		
-	}
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) cin >> prime[i];
+
+	Solve(0, 0, 1);
+	cout << ans;
+
 	return 0;
 }
